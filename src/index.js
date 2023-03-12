325cbc5d47e7a07f65ad5c8b4daab1a383b63c75
korch90 <38561125+korch90@users.noreply.github.com>
2023-03-12 18:43:18 +0200

    f

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
  import { HashRouter} from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById('root'));
+<meta http-equiv="Permissions-Policy" content="interest-cohort=()"></meta>
 root.render(
   <React.StrictMode>
 
